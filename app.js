010a7445fc54c81c0fed8c4d6e1b68b59f2549e6
Kevin Ho <ho.kevins@gmail.com>
2017-11-07 11:47:17 -0500

    Start tweetBank

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
 const express = require('express');
-const morgan = require('morgan');
 const app = express(); // creates an instance of an express application
-
+const morgan = require('morgan');
+const nunjucks = require('nunjucks');
 const chalk = require('chalk');
 
-app.use(function (req, res, next) {
-    // do your logging here
+// SIDE NOTE: notice how our app.js script depends on numerous modules (Express, Morgan, Nunjucks, etc.) but tweetBank.js only depends on one (Lodash). Connecting dependencies together with a module system makes it much easier to grasp the overall structure and complexity of an application.
+
+// Connecting nunjucks to express so Express can send completed HTML rendered by Nunjucks as a response to Browser request.
+
+app.set('view engine', 'html'); // have res.render work with html files
+app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
+nunjucks.configure('views'); // point nunjucks to the proper directory for templates
+
+// Middleware, has to come first:
+app.use(function (req, res, next) { // app.use will handle all HTTP verbs, get, post, delete, and put
     console.log(chalk.red('Request Type: ' + req.method + ' ' + req.originalUrl + ' ' + res.statusCode));
     // call `next`, or else your app will be a black hole — receiving requests but never properly responding
-    next();
+    next(); // next tell Express to find the next route and move on once function runs or else will stall the page.  You must either call next or send a response back and the route is over.
 })
 
 app.get('/', function(req, res) {
-  res.send('Welcome to our faux-Twitter!');
+  res.render( 'index', {title: 'Hall of Fame', people: localVars.people} );
+  // res.reder also sends completed HTML documents as a response.
 });
 
 app.get('/news', function(req, res) {
@@ -24,3 +33,18 @@ app.listen(3000, function() {
 });
 
 // if (err) throw err; for all types of Node-style callback function.  Known as "errbacks", to prevent silent errors which are difficult to debug.
+
+var localVars = {
+  title: 'An Example',
+  people: [
+    { name: 'Gandalf'},
+    { name: 'Frodo'},
+    { name: 'Hermione'},
+  ]
+};
+
+nunjucks.configure('views', {noCache: true}); // noCache (default: false) never use a cache and recompile templates each time (server-side)
+nunjucks.render('index.html', localVars, function(err, output) {
+  if (err) throw err;
+  console.log(output);
+});
