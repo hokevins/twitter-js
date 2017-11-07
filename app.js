b102e2e35a4ce0689d22058f73ca6106f9225abf
Kevin Ho <ho.kevins@gmail.com>
2017-11-06 19:07:21 -0500

    2017-11-06

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
-const express = require( 'express' );
+const express = require('express');
+const morgan = require('morgan');
 const app = express(); // creates an instance of an express application
 
-app.listen(3000, function() {
-  console.log('server listening on Port 3000');
-});
+const chalk = require('chalk');
+
+app.use(function (req, res, next) {
+    // do your logging here
+    console.log(chalk.red('Request Type: ' + req.method + ' ' + req.originalUrl + ' ' + res.statusCode));
+    // call `next`, or else your app will be a black hole — receiving requests but never properly responding
+    next();
+})
 
 app.get('/', function(req, res) {
   res.send('Welcome to our faux-Twitter!');
 });
 
+app.get('/news', function(req, res) {
+  res.send('Here\'s the news.');
+});
+
+app.listen(3000, function() {
+  console.log('server listening on Port 3000');
+});
